d6ffd5391c7f222abc81382cd79b12833ea03b2f
Patricio Soto Kort <psotokort@gmail.com>
2024-08-26 19:39:21 +0800

    Add parentesis to Contains

--- NormalizeTextColumns.m
+++ NormalizeTextColumns.m
@@ -28,7 +28,7 @@ let
                     (state, current) =>
                         let
                             PrevLine = if state = "" then "" else Text.End(state, 1),
-                            CurrentStartsWithSymbol = Text.Start(current, 1) <> "" and not Text.Contains("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", Text.Start(current, 1)),
+                            CurrentStartsWithSymbol = Text.Start(current, 1) <> "" and not Text.Contains("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789(", Text.Start(current, 1)),
                             PrevEndsWithPeriod = PrevLine = ".",
                             NewLineSeparator = if CurrentStartsWithSymbol or PrevEndsWithPeriod then "#(lf)" else " "
                         in
